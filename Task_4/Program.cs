0476a711ad9adecaf91177536a439fc366d6717b
ODIN74 <fantompus@gmail.com>
2018-04-28 17:55:34 +0500

    Начал делать 4 задание

---
+++ Task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwodimensionalArray arr = new TwodimensionalArray(4, 5, -10, 20);
+
+            DisplayToConsole(arr);
+            Console.ReadLine();
+        }
+
+        public void DisplayToConsole(TwodimensionalArray array)
+        {
+            string line = String.Empty;
+
+            for (int i = 0; i < array.LengthByLines; i++)
+            {
+                line = String.Empty;
+                for (int j = 0; j < array.LengthByColumns; j++)
+                {
+                    line += " " + array[i, j];
+                }
+                Console.WriteLine("\n" + line);
+            }
+
+        }
+    }
+}
---
+++ Task_4/TwodimensionalArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class TwodimensionalArray
+    {
+        private int[,] array;
+
+        public TwodimensionalArray(int lines, int columns)
+        {
+            int[,] array = new int[lines, columns];
+        }
+
+        public TwodimensionalArray(int lines, int columns, int min, int max)
+        {
+            int[,] array = new int[lines, columns];
+            Random rnd = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(min, max);
+                }
+            }
+        }
+
+        public TwodimensionalArray this[int index0, int index1]
+        {
+            get { return array[index0, index1]; }
+        }
+
+        public int LengthByLines 
+        {
+            get { return array.GetLength(0); }
+        }
+
+        public int LengthByColumns
+        {
+            get { return array.GetLength(1); }
+        }
+
+
+    }
+}
